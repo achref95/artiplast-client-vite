76c57065c8fc503b57f582dd91c6aae2dd6e0ca5
Achref LAABIDI <abidi.achref95@gmail.com>
2023-10-30 18:26:47 +0100

    added link to navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import { Link } from "react-router-dom"
 
 const Nav = () => {
   return (
+    <Link to="/">
       <div className="navbar bg-neutral text-neutral-content mb-4">
         <a className="btn btn-ghost normal-case text-xl">ARTIPLAST</a>
       </div>
+    </Link>
   )
 }
 
